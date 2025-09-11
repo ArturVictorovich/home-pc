34cc9d2363deb8248d7957fecde297f4e5bc0fe4
Artur Victorovich <mostvad7777@gmail.com>
2025-09-11 17:25:35 +0700

    feat: дедал адаптив header на мобилку, начал на планшет

--- pet_projects/web_store_app/src/components/layout/Header/Header.tsx
+++ pet_projects/web_store_app/src/components/layout/Header/Header.tsx
@@ -3,47 +3,96 @@ import { RxHamburgerMenu } from 'react-icons/rx';
 import { RiSearchLine } from 'react-icons/ri';
 import { GoHeart } from 'react-icons/go';
 import { IoCartOutline } from 'react-icons/io5';
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
 const Header = () => {
+  const [burger, setBurger] = useState<boolean>(false);
   return (
-    <header>
+    <header className="w-full">
       {/* топ header */}
-      <section className="bg-black  h-7 relative flex justify-center  items-center">
-        <h1 className="text-white  font-regular text-sm">
-          Summer Sale For All Swim Suits And Free Express Delivery - OFF 50%!
-        </h1>
-        <button className="text-white mr-2 font-semibold cursor-pointer hover:underline ">
-          ShopNow
-        </button>
-        <button className="text-white text-sm absolute right-1 flex items-center">
+      <section
+        className="bg-black px-4 h-8 relative flex justify-start items-center
+        md:px-11.75 md:justify-between "
+      >
+        <div className="flex flex-row items-center">
+          <h1 className="text-white me-1 font-regular text-sm">
+            Summer Sale For All Swim Suits And Free Express Delivery - OFF 50%!
+          </h1>
+          <Link
+            to="*"
+            className="text-white mr-2  font-semibold cursor-pointer hover:underline "
+          >
+            ShopNow
+          </Link>
+        </div>
+
+        <button
+          className="text-white text-sm flex items-center absolute right-4
+        md:static"
+        >
           English <FaChevronDown />
         </button>
       </section>
 
       {/* навигация */}
-      <section className="flex px-5 h-9.5 justify-between">
-        <div className="text-2xl font-bold">Exclusive</div>
+      <section
+        className="flex px-4 h-12 justify-between items-center
+      md:px-11.75
+      "
+      >
+        <div className="flex items-center">
+          {/* бургер */}
+          <button className="pointer mr-3 md:hidden ">
+            <RxHamburgerMenu className="size-8" />
+          </button>{' '}
+          <div
+            className="text-2xl mr-10 font-bold align-items:center
+          md:mr-0"
+          >
+            Exclusive
+          </div>
+        </div>
+        <ul className="">
+          <Link className="mr-3" to="*">
+            Home
+          </Link>
+          <Link className="mr-3" to="*">
+            Contact
+          </Link>
+          <Link className="mr-3" to="*">
+            About
+          </Link>
+          <Link to="*">Sign Up</Link>
+        </ul>
         <div className="flex">
-          <form className="">
+          <form className="relative mr-3">
             <input
-              className="rounded h-9.5 w-60.75 bg-white/10"
+              className="rounded shadow h-7 p-1  w-60.75 bg-black/10
+              md:w-55"
               placeholder="What are you looking for?"
               type="seacrch"
             />
-
-            <button className="right-1">
-              <RiSearchLine />
+            <button
+              type="submit"
+              title="search"
+              className="h-7 absolute right-2"
+            >
+              <RiSearchLine className="size-5" />
             </button>
           </form>
-          <button className="mr-2">
-            <GoHeart />
-          </button>
-          <button className="mr-8">
-            <IoCartOutline />
-          </button>
-          <button className="text-2xl ">
-            <RxHamburgerMenu />
-          </button>
+          <Link to="*" className="mr-2">
+            <GoHeart className="size-7" />
+          </Link>
+          <Link to="*">
+            <IoCartOutline className="size-7" />
+          </Link>
         </div>
+        {/* <ul className="flex bg absolute top-15 flex-col">
+          <Link to="*">Home</Link>
+          <Link to="*">Contact</Link>
+          <Link to="*">About</Link>
+          <Link to="*">Sign Up</Link>
+        </ul> */}
       </section>
     </header>
   );
